3c94981935f40f8e8b677b0beed28b2765d0d899
miakahira <miastraumann@icloud.com>
2025-12-02 13:52:30 +0100

    Hauptdateiten für Pokedex hinzugefügt

---
+++ script.js
@@ -0,0 +1,84 @@
+// Globale Variablen
+const pokemonBilderQuelle = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+const pokemons = [
+  { name: "Pikachu", type: "Elektro", image: `${pokemonBilderQuelle}25.png` },
+  { name: "Glumanda", type: "Feuer", image: `${pokemonBilderQuelle}4.png` },
+  { name: "Schiggy", type: "Wasser", image: `${pokemonBilderQuelle}7.png` },
+  { name: "Bisasam", type: "Pflanze", image: `${pokemonBilderQuelle}1.png` },
+  { name: "Evoli", type: "Normal", image: `${pokemonBilderQuelle}133.png` },
+  {name: "Vulpix", type:"Feuer", image:`${pokemonBilderQuelle}37.png`},
+  {name: "Myrapla", type:"Pflanze", image:`${pokemonBilderQuelle}43.png`},
+  {name: "Enton", type:"Wasser", image:`${pokemonBilderQuelle}54.png`},
+  {name: "Dratini", type:"Drache", image:`${pokemonBilderQuelle}147.png`},
+  {name: "Mogelbaum", type:"Stein", image:`${pokemonBilderQuelle}185.png`},
+];
+
+
+// HTML-Elemente
+let firstSection = document.createElement("div");
+firstSection.id = "title-section";
+firstSection.classList.add("container");
+document.body.appendChild(firstSection);
+
+let title = document.createElement("h1");
+title.id = "title";
+title.innerText = "Pokédex";
+firstSection.appendChild(title);
+
+let pokemonCards = document.createElement("div");
+pokemonCards.id = "pokemon-cards";
+pokemonCards.classList.add("container");
+document.body.appendChild(pokemonCards);
+
+
+for (const pokemon of pokemons){
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const title = document.createElement("h2");
+    title.textContent = pokemon.name;
+
+    const image = document.createElement("img");
+    image.src = pokemon.image;
+
+    const type = document.createElement("p");
+    type.textContent = "Typ: " + pokemon.type;
+    if (pokemon.type=="Feuer"){
+        type.style.color="red";
+    }
+    else{
+        if(pokemon.type=="Wasser"){
+            type.style.color="blue";
+        }
+        else{
+            if(pokemon.type=="Pflanze"){
+                type.style.color="green";
+            }
+        }
+    }
+
+    card.appendChild(title);
+    card.appendChild(image);
+    card.appendChild(type);
+
+    pokemonCards.appendChild(card);
+}
+
+// Funktionen
+
+
+const cities = ["Paris", "London", "Berlin"];
+
+cities.forEach(city => {
+  console.log(city);
+  console.log(city.length);
+});
+
+
+
+
+
+
+// Event-Listeners
