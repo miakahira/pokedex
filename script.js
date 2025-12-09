8ba0a4dfc706fce86d21dfcf782ee84ccd4fe5e2
miakahira <miastraumann@icloud.com>
2025-12-09 15:57:09 +0100

    Funktion für API-Calls hinzufügen (refs #13)

--- script.js
+++ script.js
@@ -99,8 +99,22 @@ function displayAllPokemon(){
     pokemonDetailView.classList.toggle("hidden");
     pokemonDetailView.innerHTML=("");
 }
-// Event-Listeners
 
+async function getData(apiEndpoint){
+    try{
+        const response = await fetch(apiENdpoint);
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`)
+        }
+        const result = await response.json();
+        return result;
+    } catch (error){
+        console.error(error.message);
+    }
+}
+
+
+// Event-Listeners
 title.addEventListener("click", function(){
     console.log("Titel wurde geklickt")
     if (pokemonCards.classList.contains("hidden")){
